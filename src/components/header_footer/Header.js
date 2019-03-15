5ae0d9b2043659e99ba781debab14c708266216a
augustnejudne <august.nejudne@gmail.com>
2019-03-15 16:25:10 +0800

    added header and menu drawer

---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import SideDrawer from './SideDrawer';
+
+class Header extends Component {
+  state = {
+    drawerOpen: false,
+  };
+
+  toggleDrawer = value => {
+    this.setState({
+      drawerOpen: value,
+    });
+  };
+
+  render() {
+    return (
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: '#2f2f2f',
+          boxShadow: 'none',
+          padding: '10px 0',
+        }}
+      >
+        <Toolbar>
+          <div className="header_logo">
+            <div className="font_righteous header_logo_venue">The Venue</div>
+            <div className="header_logo_title">Musical Events</div>
+          </div>
+
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={() => this.toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <SideDrawer
+            open={this.state.drawerOpen}
+            onClose={value => this.toggleDrawer(value)}
+          />
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default Header;
